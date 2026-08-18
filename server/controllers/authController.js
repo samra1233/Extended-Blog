e3b5da158a38113c691e1da0bf52ea8d178661f7
samra1233 <samra20020413@gmail.com>
2026-08-19 00:59:43 +0500

    Auto assign admin role to first user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,7 +35,10 @@ export const registerUser = asyncHandler(async (req, res) => {
     throw new Error('User with that email already exists');
   }
 
-  const user = await User.create({ name, email, password });
+  const userCount = await User.countDocuments();
+  const role = userCount === 0 ? 'admin' : 'user';
+
+  const user = await User.create({ name, email, password, role });
   res.status(201).json(formatUser(user, generateToken(user._id)));
 });
 
